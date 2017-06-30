b24040196e8c17611f41bab815380f6b3aa5a133
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-06-30 15:35:31 -0400

    add exercise 1  bankaccount.rb

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# object_oriented_programming
-intro assignment to enforce understanding of object oriented programming
+1. Create a BankAccount class
+
+2. Every bank account should have balance and interest_rate attributes.
+At this point you should test out creating an instance of your
+class to make sure it works.
+
+3. Your class should have an instance method called deposit that accepts
+one amount argument and adds that amount to the total balance.
+Test out your method by calling it on an instance of your class.
+
+4. There should be a withdraw instance method that accepts one amount
+argument and subtracts it from the total balance.
+Don't forget to test it out!
+
+5. Finally, there should be a gain_interest instance method that
+increases the total balance according to the interest rate.
+Once all of that is working, don't forget to commit!
--- bankaccount.rb
+++ bankaccount.rb
@@ -1,4 +1,52 @@
+#create class
 class BankAccount
-  @balance
-  @interest_rate
+
+#INSTANCE METHODS and class attributes
+
+    #INSTANCE READER METHODS
+    def initialize(balance, interest_rate)
+      @balance = balance
+      @interest_rate = interest_rate
+    end
+
+    def balance
+      return @balance
+    end
+
+    def interest_rate
+      return @interest_rate
+    end
+
+    #INSTANCE WRITER METHODS
+    def deposit(amount)
+      @balance += amount
+    end
+
+    def withdraw(amount)
+      @balance -= amount
+    end
+
+    def gain_interest(rate)
+      rate = @interest_rate
+      rate * balance = @balance
+
+      rate == @balance * @interest_rate
+    end
 end
+
+BankAccount.new(0, 0.2)
+
+AndrewsAccount = BankAccount.new(300, 0.2)
+
+AndrewsAccount.deposit(300)
+
+puts AndrewsAccount.balance
+puts AndrewsAccount.interest_rate
+
+
+
+
+
+
+
+# BankAccount.new
